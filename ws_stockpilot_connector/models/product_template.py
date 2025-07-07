5ceb3ebe38f9433ab5040c2449555d3fac543433
Insaf Amrani <insaf.amrani.boukhobza@wesolved.com>
2025-07-07 16:23:03 +0100

    [Feature][SPT-15]: Adding author

--- ws_stockpilot_connector/models/product_template.py
+++ ws_stockpilot_connector/models/product_template.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2025 WeSolved BV <https://wesolved.com>
 # @author Miro Tasevski <miro.tasevski@wesolved.com>
+# @author Insaf Amrani <insaf.amrani.boukhobza@wesolved.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
