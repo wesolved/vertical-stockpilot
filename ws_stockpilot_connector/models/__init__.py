b5a649e536e97ae5e4e22cfc9508c0c4f716997b
Robin Conjour <robin.conjour@wesolved.com>
2025-08-07 15:33:04 +0200

    [FIX] Pre-commit and scheduled actions

--- ws_stockpilot_connector/models/__init__.py
+++ ws_stockpilot_connector/models/__init__.py
@@ -8,3 +8,5 @@ from . import sale_order_line
 from . import stock_move
 from . import stock_picking
 from . import stockpilot_configuration
+from . import stockpilot_product_product
+from . import stockpilot_product_template
