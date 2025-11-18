215dfefacde3cb41f05a008c38f7df0e393f4ede
Robin Conjour <robin.conjour@wesolved.com>
2025-11-18 19:12:55 +0100

    [UPD] 19.0

--- ws_stockpilot_warehouse/__manifest__.py
+++ ws_stockpilot_warehouse/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stockpilot Connector - Warehouse Selection",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "summary": "Map countries with specific warehouses for Stockpilot orders",
     "author": "WeSolved B.V.",
     "website": "https://wesolved.com",
