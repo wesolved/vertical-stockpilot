8df894f103a562517b0744647892b1b7235f589a
Robin Conjour <robin.conjour@wesolved.com>
2026-03-09 14:28:25 +0100

    [ADD] Fix stockpilot

--- ws_stockpilot_connector/models/__init__.py
+++ ws_stockpilot_connector/models/__init__.py
@@ -7,6 +7,6 @@ from . import sale_order_line
 from . import stock_move
 from . import stock_picking
 from . import stockpilot_configuration
-from . import stockpilot_product_category
+# from . import stockpilot_product_category
 from . import stockpilot_product_product
 from . import stockpilot_product_template
